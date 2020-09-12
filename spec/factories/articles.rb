bc80309deb249fe419044f73308318cb95c82ad7
kojikoji00 <y_koji_0807@yahoo.co,jp>
2020-09-12 16:24:09 +0900

    deviseの導入

---
+++ spec/factories/articles.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :article do
+    
+  end
+end
