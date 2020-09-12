e8b27bf53a05026d579c77fdcaa2a1685ac04077
kojikoji00 <y_koji_0807@yahoo.co,jp>
2020-09-13 07:03:04 +0900

    プロフィールモデルを作成

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+require 'rails_helper'
+
+RSpec.describe Profile, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
