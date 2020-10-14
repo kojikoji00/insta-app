2e619768a9090f32f3cd0a41c581c3204069b796
kojikoji00 <y_koji_0807@yahoo.co,jp>
2020-10-14 14:39:23 +0900

    動的にIDが付与できない

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,27 +1,53 @@
 import $ from 'jquery'
 import axios from 'modules/axios'
-import {
-  listenInactiveHeartEvent,
-  listenActiveHeartEvent
-} from 'modules/handle_heart'
 
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked) {
-    $('.active-heart').removeClass('hidden')
+    $('.active-heart${articleId}').removeClass('hidden')
   } else {
-    $('.inactive-heart').removeClass('hidden')
+    $('.inactive-heart${articleId}').removeClass('hidden')
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const dataset = $('#article-show').data()
-  const articleId = dataset.articleId
+  var dataset = $('.article-show').attr('id')
+  // var dataset = $('.article-show').attr('id', 'box')
+  var dataset = $('.article-show').data()
+  var articleId = dataset.articleId
+  debugger
   axios.get(`/articles/${articleId}/like`)
+  .then((response) => {
+    const hasLiked = response.data.hasLiked
+    if (hasLiked) {
+      $('.active-heart').removeClass('hidden')
+    } else {
+      $('.inactive-heart').removeClass('hidden')
+    }
+  })
+  $('.inactive-heart').find('articleId').on('click', () => {
+    axios.post(`/articles/${articleId}/like`)
     .then((response) => {
-      const hasLiked = response.data.hasLiked
-      handleHeartDisplay(hasLiked)
+      if (response.data.status === 'ok') {
+        $('.active-heart').find('articleId').removeClass('hidden')
+        $('.inactive-heart').find('articleId').addClass('hidden')
+      }
     })
-  listenInactiveHeartEvent(articleId)
-  listenActiveHeartEvent(articleId)
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+    })
+    $('.active-heart').find('articleId').on('click', () => {
+      axios.delete(`/articles/${articleId}/like`)
+      .then((response) => {
+        if (response.data.status === 'ok') {
+          $('.active-heart').find('articleId').addClass('hidden')
+          $('.inactive-heart').find('articleId').removeClass('hidden')
+        }
+      })
+      .catch((e) => {
+        window.alert('Error')
+        console.log(e)
+      })
+  })
 })
-// })
